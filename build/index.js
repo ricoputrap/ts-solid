e1bcf98d77e3cd575e7a82792455eda4ba1a6c1c
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-05-22 16:03:13 +0700

    remove build folders

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// import mainSRP from "./single-responsibility-principle/Shape";
-const open_closed_principle_1 = __importDefault(require("./open-closed-principle"));
-// mainSRP();
-(0, open_closed_principle_1.default)();
--- build/single-responsibility-principle/Book/BookSaving.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BookSaving {
-    static saveBook(book) {
-        console.log("save this book:", book);
-    }
-}
-exports.default = BookSaving;
